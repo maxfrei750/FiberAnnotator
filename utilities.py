7b9b17bd250ab4a5e5e4af0a188b41fbee40020e
Max Frei <max.frei@uni-due.de>
2019-10-11 17:45:37 +0200

    Streamlined code.

--- utilities.py
+++ utilities.py
@@ -3,20 +3,16 @@ from scipy import interpolate
 
 
 def spline_interpolation(coordinates):
+    n_interpolation_steps = 8
+
     if not coordinates or coordinates is None:
         return None
 
     n_coordinates = len(coordinates)
 
     if n_coordinates >= 4:
-        coordinates = np.array(coordinates)
-
-        x = coordinates[:, 0]
-        y = coordinates[:, 1]
-
-        tck, _ = interpolate.splprep([x, y], s=0)
-
-        n_interpolation_steps = 8
+        coordinates = np.array(coordinates).transpose()
+        tck, _ = interpolate.splprep(coordinates, s=0)
         x_new, y_new = interpolate.splev(np.linspace(0, 1, n_coordinates * n_interpolation_steps), tck, der=0)
 
         return np.stack((x_new, y_new), axis=-1).tolist()
