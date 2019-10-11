d3555bfe7559b14f7366821865feea32703bff0d
Max Frei <max.frei@uni-due.de>
2019-10-11 14:38:20 +0200

    Stage handle cases where not enough points were given.

--- utilities.py
+++ utilities.py
@@ -3,9 +3,12 @@ from scipy import interpolate
 
 
 def spline_interpolation(coordinates):
+    if not coordinates or coordinates is None:
+        return None
+
     n_coordinates = len(coordinates)
 
-    if n_coordinates > 3:
+    if n_coordinates >= 4:
         coordinates = np.array(coordinates)
 
         x = coordinates[:, 0]
@@ -15,3 +18,5 @@ def spline_interpolation(coordinates):
         x_new, y_new = interpolate.splev(np.linspace(0, 1, n_coordinates * 12), tck, der=0)
 
         return np.stack((x_new, y_new), axis=-1).tolist()
+    else:
+        return coordinates
